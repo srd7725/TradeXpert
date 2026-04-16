afe3acb3f2670c490f73e55b17e01ac85aafd169
Srd kumar <sharadkumar7725@gmail.com>
2026-04-17 01:04:00 +0530

    Working on products page

--- frontend/src/landing_page/product/Hero.js
+++ frontend/src/landing_page/product/Hero.js
@@ -1,7 +1,13 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+       <div className='container border-bottom mb-5'>
+        <div className='text-center mt-5 p-3'>
+       <h1>Zerodha Products</h1>
+       <h3 className='text-muted mt-3 fs-4'>Sleek, modern, and intuitive trading platforms</h3>
+       <p className='mt-3 mb-5'>Check out our <a href='' style={{textDecoration:"none"}}>investment offerings →</a> </p>
+       </div>
+       </div>
      );
 }
 
--- frontend/src/landing_page/product/LeftSection.js
+++ frontend/src/landing_page/product/LeftSection.js
@@ -1,8 +1,33 @@
 import React from 'react';
-function LeftSection() {
-    return ( 
-        <h1>LeftSection</h1>
-     );
+function LeftSection({
+    imageURL, 
+    productName, 
+    productDescription, 
+    tryDemo, 
+    learnMore, 
+    googlePlay, 
+    appStore})
+    {
+    return <div className='container mt-5'>
+        <div className='row'>
+            <div className='col-6'>
+                <img src={imageURL} alt='product'/>
+            </div>
+            <div className='col-6 p-5 mt-5'>
+                <h1>{productName}</h1>
+                <p>{productDescription}</p>
+                <div>
+                <a href={tryDemo} style={{textDecoration:"none"}}>Try Demo <i class="fa-solid fa-arrow-right"></i></a>
+                <a href={learnMore} style={{marginLeft:"50px", textDecoration:"none"}} >Learn More <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+
+                <div className='mt-4'>
+                <a href={googlePlay}><img src='media/images/googlePlayBadge.svg'/></a>
+                <a href={appStore}  style={{marginLeft:"50px"}}><img src='media/images/appStoreBadge.svg'/></a>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default LeftSection;
