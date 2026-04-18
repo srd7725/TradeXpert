9c05006319a2d03b51e4e5d0757fe1b3dccf52a2
Srd kumar <sharadkumar7725@gmail.com>
2026-04-19 02:43:55 +0530

    update hero file of support

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,43 +2,38 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="container-fluid" id="supportHero">
-      <div className="p-5" id="supportWrapper">
-        <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
-      </div>
-
-      <div className="row p-5 m-3">
-        <div className="col-6 p-3">
-          <h1 className="fs-3">
-            Search for an answer or browse help topics to create a ticket
-          </h1>
-
-          <input
-            className="form-control my-3"
-            placeholder="Eg. how do I activate F&O"
-          />
-
-          <a href="">Track account opening</a><br />
-          <a href="">Track segment activation</a><br />
-          <a href="">Intraday margins</a><br />
-          <a href="">Kite user manual</a>
+     <section className="container-fluid" id="supportHero">
+         <div className="container">
+         <div className="d-flex justify-content-between align-items-center py-5">
+         <h4>Support Portal</h4>
+          <a href="">Track Tickets</a>
+         </div>
         </div>
+        <div className="row p-5 m-3 gx-5">
+           <div className="col-6 p-5 ">
+            <h1 className="fs-3">Search for an answer or browse help topics to create a ticket </h1>
+            <input placeholder="Eg: How do I open my account, How do i activate F&O..." className="mb-3"/>
+            <br/>
+            <a href="">Track account opening</a>
+            <a href="">Track segment activation</a>
+            <a href="">Intraday margins</a>
+            <a href="">Kite user manual</a>
 
-        <div className="col-6 p-3">
-          <h1 className="fs-3">Featured</h1>
+           </div>
+          <div className="col-md-5 offset md-1 mt-5">
+            <h1  className="fs-3">Featured</h1>
+            <ol>
+              <li><a href="">Current Takeovers and Delisting - January 2025</a></li>
+              <li><a href="">Latest Intraday leverages - MIS & CO</a></li>
+            </ol>
+             
+            
 
-          <ol>
-            <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
-            </li>
-            <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
-            </li>
-          </ol>
+           </div>
+        
+
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
 
