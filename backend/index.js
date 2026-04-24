6075ed7919bdff95b30e47a52157d780241117c2
Srd kumar <sharadkumar7725@gmail.com>
2026-04-25 03:59:06 +0530

    positions data insert in db

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const {HoldingsModel} = require ("./model/HoldingsModel");
+const PositionsModel = require("./model/PositionsModel");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
@@ -139,6 +140,71 @@ const app = express();
 //     res.send("Done!");
 
 // });
+
+app.get("/addPositions", async (req, res) => {
+  let tempPositions = [
+    {
+      product: "CNC",
+      name: "EVEREADY",
+      qty: 2,
+      avg: 316.27,
+      price: 312.35,
+      net: "+0.58%",
+      day: "-1.24%",
+      isLoss: true,
+    },
+    {
+      product: "CNC",
+      name: "JUBLFOOD",
+      qty: 1,
+      avg: 3124.75,
+      price: 3082.65,
+      net: "+10.04%",
+      day: "-1.35%",
+      isLoss: true,
+    },
+  ];
+
+  tempPositions.forEach((item) => {
+    let newPosition = new PositionsModel({
+      product: item.product,
+      name: item.name,
+      qty: item.qty,
+      avg: item.avg,
+      price: item.price,
+      net: item.net,
+      day: item.day,
+      isLoss: item.isLoss,
+    });
+
+    newPosition.save();
+  });
+  res.send("Done!");
+});
+
+// app.get("/allHoldings", async (req, res) => {
+//   let allHoldings = await HoldingsModel.find({});
+//   res.json(allHoldings);
+// });
+
+// app.get("/allPositions", async (req, res) => {
+//   let allPositions = await PositionsModel.find({});
+//   res.json(allPositions);
+// });
+
+// app.post("/newOrder", async (req, res) => {
+//   let newOrder = new OrdersModel({
+//     name: req.body.name,
+//     qty: req.body.qty,
+//     price: req.body.price,
+//     mode: req.body.mode,
+//   });
+
+//   newOrder.save();
+
+//   res.send("Order saved!");
+// });
+
 
 app.listen(PORT, () => {
   console.log("App started!");
--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -1,7 +1,7 @@
-const { model } = require("mongoose");
+const mongoose = require("mongoose");
 
-const { PositionsModel } = require("../schemas/PositionsModel");
+const { PositionsSchema } = require("../schemas/PositionsSchema");
 
-const PositionsModel = new model("position", PositionsModel);
+const PositionsModel = mongoose.model("Position", PositionsSchema);
 
-module.exports = { PositionsModel };
+module.exports = PositionsModel;
